d8c59392392752cfe2d3019b761dc57e297faa1c
npopli <namratapopli3@gmail.com>
2018-05-04 12:44:28 +0530

    files updated

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import{ Recipe} from '../recipe.model';
+import { Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,7 +8,7 @@ import{ Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
     recipes: Recipe[] = [
-      new Recipe ('A Test Recipe', 'this is a text recipe', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg')
+      new Recipe ('A Test Recipe', 'this is a text recipe', 'http://weclipart.com/gimg/7BFD8A19DF89EA32/1-retro-cookbook-clipart9.jpg')
     ];
   constructor() { }
 
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+/* export class Ingredient {
+    public name: String;
+    public amount: number;
+    constructor (name: string, amount: number) {
+        this.name = name;
+        this.amount =  amount;
+     }
+    // tslint:disable-next-line:eofline
+    } */
+
+export class Ingredient {
+constructor (public name: string, public amount: number ) {}
+// tslint:disable-next-line:eofline
+}
